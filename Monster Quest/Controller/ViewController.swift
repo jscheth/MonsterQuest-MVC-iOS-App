c670f353d28aa3b790a37149b872043b2c6a5572
jscheth <sovichea.chet@gmail.com>
2025-04-01 15:31:57 -0700

    Resolve some issues

--- Monster Quest/Controller/ViewController.swift
+++ Monster Quest/Controller/ViewController.swift
@@ -13,16 +13,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyLogic = StoryLogic()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //updateUI()
     }
     
-    
-    @IBAction func choicePressed(_ sender: UIButton) {
-        
-        
-    }
-    
+//    @IBAction func choicePressed(_ sender: UIButton) {
+//        storyLogic.nextStory(userChoice: sender.currentTitle!)
+//        updateUI()
+//    }
+//    
+//    func updateUI() {
+//        storyTextLabel.text = storyLogic.getStoryTitle()
+//        choice1Button.setTitle(storyLogic.getChoice1(), for: .normal)
+//        choice2Button.setTitle(storyLogic.getChoice2(), for: .normal)
+//    }
 
 }
 
---
+++ Monster Quest/Model/Story.swift
@@ -0,0 +1,14 @@
+//
+//  Story.swift
+//  Monster Quest
+//
+//  Created by Jonathan Cheth on 4/1/25.
+//
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+}
