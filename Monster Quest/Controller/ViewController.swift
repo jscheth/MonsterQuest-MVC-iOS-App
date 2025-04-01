9acc22bde47bf730174b648e2e263c0cb2e1390b
jscheth <sovichea.chet@gmail.com>
2025-04-01 15:00:10 -0700

    Create the layout

--- Monster Quest/Controller/ViewController.swift
+++ Monster Quest/Controller/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var storyTextLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func choicePressed(_ sender: UIButton) {
+        
+        
+    }
+    
 
 }
 
