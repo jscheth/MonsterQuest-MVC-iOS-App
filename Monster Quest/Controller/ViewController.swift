52e0dd0afd9444243b25e2fb44e07406076b0fcc
jscheth <sovichea.chet@gmail.com>
2025-04-01 15:39:41 -0700

    Resolve issues with the plist

--- Monster Quest/Controller/ViewController.swift
+++ Monster Quest/Controller/ViewController.swift
@@ -9,27 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var storyTextLabel: UILabel!
-    @IBOutlet weak var choice1Button: UIButton!
-    @IBOutlet weak var choice2Button: UIButton!
+    @IBOutlet var storyTextLabel: UILabel!
+    
+    @IBOutlet var choice1Button: UIButton!
+    
+    @IBOutlet var choice2Button: UIButton!
     
     var storyLogic = StoryLogic()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //updateUI()
     }
     
-//    @IBAction func choicePressed(_ sender: UIButton) {
-//        storyLogic.nextStory(userChoice: sender.currentTitle!)
-//        updateUI()
-//    }
-//    
-//    func updateUI() {
-//        storyTextLabel.text = storyLogic.getStoryTitle()
-//        choice1Button.setTitle(storyLogic.getChoice1(), for: .normal)
-//        choice2Button.setTitle(storyLogic.getChoice2(), for: .normal)
-//    }
-
+    @IBAction func choicePressed(_ sender: UIButton) {
+        storyLogic.nextStory(userChoice: sender.currentTitle!)
+        
+        updateUI()
+    }
+    
+    func updateUI() {
+        storyTextLabel.text = storyLogic.getStoryTitle()
+        choice1Button.setTitle(storyLogic.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyLogic.getChoice2(), for: .normal)
+    }
+    
 }
 
