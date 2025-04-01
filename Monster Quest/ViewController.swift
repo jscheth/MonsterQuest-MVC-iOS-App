41cf32f4d8748bfa17f57f803f6ba37fe3d4abfc
jscheth <sovichea.chet@gmail.com>
2025-04-01 12:52:07 -0700

    Initial Commit

---
+++ Monster Quest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Monster Quest
+//
+//  Created by Jonathan Cheth on 4/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Monster QuestTests/Monster_QuestTests.swift
@@ -0,0 +1,17 @@
+//
+//  Monster_QuestTests.swift
+//  Monster QuestTests
+//
+//  Created by Jonathan Cheth on 4/1/25.
+//
+
+import Testing
+@testable import Monster_Quest
+
+struct Monster_QuestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
